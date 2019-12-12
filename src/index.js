4a2177ad56a21793343e0cab18e8250148f35f65
Slowpokita <gr3c2l@gmail.com>
2019-12-12 02:35:50 -0300

    scafolding, webpack and scss mixins

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import { render } from 'react-dom'
+
+//scss
+import './styles/main.scss'
+
+//PAGES
+
+//MOBILE CLICK MENU 
+const App = () => (
+  <div></div>
+)
+
+render(<App/>, document.getElementById('root'))
